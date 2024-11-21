95c2e171d98d595ec5ca97bba842bfb7f2785dca
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-11-21 10:55:33 +0530

    fixes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -28,6 +30,6 @@ app.get("/", (req, res) => {
   res.render("product", { product });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running");
 });
